591cca913a34bf69444d021abc71160b6d83a2be
jdichh <jpsdichoso21@gmail.com>
2023-10-12 01:07:06 +0800

    fixed pwa, disabled unescaped entities rule

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+  });
+  
+  const nextConfig = {};
+  
+  module.exports = withPWA({
+    ...nextConfig,
+  });
+  
