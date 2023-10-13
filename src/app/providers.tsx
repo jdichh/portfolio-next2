c39e8de3b22cba2c648d12082f4584990cd1d10b
jdichh <jpsdichoso21@gmail.com>
2023-10-13 09:20:29 +0800

    added framer page transitions

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,21 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider defaultTheme="system" attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <AnimatePresence mode="wait">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 1 }}
+          transition={{ duration: 0.25 }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </ThemeProvider>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import "../app/globals.css";
 import type { AppProps } from "next/app";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} className="max-w-[800px] mx-auto"/>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.pathname} className="h-full flex flex-col justify-between">
+          <Component {...pageProps} className="max-w-[800px] mx-auto" />
+          <motion.div
+            initial={{ scaleX: 0 }}
+            animate={{ scaleX: 0 }}
+            exit={{ scaleX: 1 }}
+            transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+            className="slide-in"
+          />
+          <motion.div
+            initial={{ scaleX: 1 }}
+            animate={{ scaleX: 0 }}
+            exit={{ scaleX: 0 }}
+            transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+            className="slide-out"
+          />
+        </motion.div>
+      </AnimatePresence>
     </ThemeProvider>
   );
 }
